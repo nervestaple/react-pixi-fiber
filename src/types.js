093a49d8fa58946fd582343eb132e26e7b84fd7d
Jim McGee <nervestaple@gmail.com>
2018-03-28 23:52:29 -0400

    publishing

--- src/types.js
+++ src/types.js
@@ -1,10 +1,10 @@
 // List of types supported by ReactPixiFiber
 export const TYPES = {
-  BITMAP_TEXT: "BitmapText",
-  CONTAINER: "Container",
-  GRAPHICS: "Graphics",
-  PARTICLE_CONTAINER: "ParticleContainer",
-  SPRITE: "Sprite",
-  TEXT: "Text",
-  TILING_SPRITE: "TilingSprite",
+  BITMAP_TEXT: "bitmapText",
+  CONTAINER: "container",
+  GRAPHICS: "graphics",
+  PARTICLE_CONTAINER: "particlecontainer",
+  SPRITE: "sprite",
+  TEXT: "text",
+  TILING_SPRITE: "tilingsprite",
 };
